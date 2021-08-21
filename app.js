9b8750bd2c266159fd35b2ebfbb382bf3c8a0432
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-20 19:49:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const mobileMenu = () => {
 
 menu.addEventListener('click', mobileMenu);
 
-// Show active menue when scrolling
+// Show active menu when scrolling
 const highlightMenu = () => {
     const elem = document.querySelector('.highlight');
     const homeMenu = document.querySelector('#home-page');
@@ -22,7 +22,16 @@ const highlightMenu = () => {
     if(window.innerWidth > 960 && scrollPos < 600) {
         homeMenu.classList.add('highlight');
         aboutMenu.classList.remove('highlight');
-
+        return
+    } else if(window.innerWidth > 960 && scrollPos < 1400) {
+        aboutMenu.classList.add('highlight')
+        homeMenu.classList.remove('highlight')
+        servicesMenu.classList.remove('highlight')
+        return
+    } else if(window.innerWidth > 960 && scrollPos < 2345) {
+        aboutMenu.classList.remove('highlight')
+        servicesMenu.classList.remove('highlight')
+        return
     }
 
 }
