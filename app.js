b4f3b7eed82faec3069fb73e488806e069a44cd7
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-20 19:01:19 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
+const navLogo = document.querySelector('#navbar__logo')
 
 // Display Mobile Menu
 const mobileMenu = () => {
@@ -8,3 +9,16 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu);
+
+// Show active menue when scrolling
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight')
+    const homeMenu = document.querySelector('#home-page')
+    const aboutMenu = document.querySelector('#about-page')
+    const servicesMenu = document.querySelector('#services-page')
+    let scrollPos = window.scrollY
+
+    // adds 'highlight' class to my menu items
+    
+
+}
